89de7bd5b94205a9a8deb37bca812c75cbc28b2d
Neo Liu <farseer285@users.noreply.github.com>
2026-05-28 13:06:50 +1200

    doc: sum up total profit as results in _generateStep3Amounts()

--- test/SearchParams.t.sol
+++ test/SearchParams.t.sol
@@ -491,14 +491,24 @@ contract SearchParams is Test {
         amounts = new uint256[](maxLength);
 
         // 1e9 or above -> ZERO_DIVISION -> revert
-        // max(1e8 + 3 * n) <= 1e22, n = 4, max = 1e20  (1st: 4814 WETH + 6747 osETH)  *MAX PROFIT*
-        // max(1e7 + 3 * n) <= 1e22, n = 5, max = 1e22  (6th: 4921 WETH + 6593 osETH)
-        // max(1e6 + 3 * n) <= 1e22, n = 5, max = 1e21  (2nd: 4828 WETH + 6733 osETH)
-        // max(1e5 + 3 * n) <= 1e22, n = 5, max = 1e20  (3rd: 4807 WETH + 6745 osETH)
-        // max(1e4 + 3 * n) <= 1e22, n = 6, max = 1e22  (7th: 4623 WETH + 6851 osETH)
-        // max(1e3 + 3 * n) <= 1e22, n = 6, max = 1e21  (4th: 4782 WETH + 6751 osETH)
-        // max(1e2 + 3 * n) <= 1e22, n = 6, max = 1e20  (5th: 4797 WETH + 6730 osETH)
-        // max(1e1 + 3 * n) <= 1e22, n = 7, max = 1e22  (8th: 4921 WETH + 6535 osETH)
+        // WETH = 3883 USD, osETH = 4082 USD
+        // max(1e8 + 3 * n) <= 1e22, n = 4,  max = 1e20  (1st: 4814 WETH + 6747 osETH = 46234016 USD)
+        // max(1e7 + 3 * n) <= 1e22, n = 5,  max = 1e22  (6th: 4921 WETH + 6593 osETH = 46020869 USD)
+        // max(1e6 + 3 * n) <= 1e22, n = 5,  max = 1e21  (2nd: 4828 WETH + 6733 osETH = 46231230 USD)
+        // max(1e5 + 3 * n) <= 1e22, n = 5,  max = 1e20  (3rd: 4807 WETH + 6745 osETH = 46198671 USD)
+        // max(1e4 + 3 * n) <= 1e22, n = 6,  max = 1e22  (7th: 4623 WETH + 6851 osETH = 45916891 USD)
+        // max(1e3 + 3 * n) <= 1e22, n = 6,  max = 1e21  (4th: 4782 WETH + 6751 osETH = 46126088 USD)
+        // max(1e2 + 3 * n) <= 1e22, n = 6,  max = 1e20  (5th: 4797 WETH + 6730 osETH = 46098611 USD)
+        // max(1e1 + 3 * n) <= 1e22, n = 7,  max = 1e22  (8th: 4921 WETH + 6535 osETH = 45784113 USD)
+
+        // max(1e8 + 2 * n) <= 1e22, n = 7,  max = 1e22  (2nd: 4911 WETH + 6652 osETH = 46222877 USD)
+        // max(1e7 + 2 * n) <= 1e22, n = 7,  max = 1e21  (1st: 4823 WETH + 6739 osETH = 46236307 USD)  *MAX PROFIT*
+        // max(1e6 + 2 * n) <= 1e22, n = 8,  max = 1e22  (3rd: 4711 WETH + 6841 osETH = 46217775 USD)
+        // max(1e5 + 2 * n) <= 1e22, n = 8,  max = 1e21  (4th: 4799 WETH + 6754 osETH = 46204345 USD)
+        // max(1e4 + 2 * n) <= 1e22, n = 9,  max = 1e22  (5th: 4910 WETH + 6631 osETH = 46133272 USD)
+        // max(1e3 + 2 * n) <= 1e22, n = 9,  max = 1e21  (6th: 4807 WETH + 6721 osETH = 46100703 USD)
+        // max(1e2 + 2 * n) <= 1e22, n = 10, max = 1e22  (7th: 4662 WETH + 6838 osETH = 46015262 USD)
+        // max(1e1 + 2 * n) <= 1e22, n = 10, max = 1e21  (8th: 4769 WETH + 6731 osETH = 45993969 USD)
 
         // 10000x only works with 1e1, otherwise ZERO_DIVISION -> revert
         // max(1e1 + 4 * n) <= 1e22, n = 5, max = 1e21
